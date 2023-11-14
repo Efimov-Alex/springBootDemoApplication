7c1881136c6ba970f1d1f80e95627c37f1dbe28e
Efimov-Alex <fastic2018l@yandex.ru>
2023-11-14 18:33:31 +0300

    TestContainers 1

---
+++ src/test/java/ru/netology/springbootdemoapplication/DemoApplicationTests.java
@@ -0,0 +1,47 @@
+package ru.netology.springbootdemoapplication;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class DemoApplicationTests {
+    @Autowired
+    TestRestTemplate restTemplate;
+
+
+    private static GenericContainer<?> devapp = new GenericContainer<>("devapp")
+            .withExposedPorts(8080);
+
+    private static GenericContainer<?> prodapp = new GenericContainer<>("prodapp:latest")
+            .withExposedPorts(8081);
+
+    @BeforeAll
+    public static void setUp() {
+        devapp.start();
+        prodapp.start();
+    }
+
+    @Test
+    void returnDevapp() {
+        ResponseEntity<String> forEntity = restTemplate.getForEntity("http://192.168.99.100:"
+                + devapp.getMappedPort(8080) + "/profile", String.class);
+        System.out.println(forEntity.getBody());
+
+    }
+
+    @Test
+    void returnProdapp() {
+        ResponseEntity<String> forEntity = restTemplate.getForEntity("http://192.168.99.100:"
+                + prodapp.getFirstMappedPort() + "/profile", String.class);
+        System.out.println(forEntity.getBody());
+    }
+
+}
